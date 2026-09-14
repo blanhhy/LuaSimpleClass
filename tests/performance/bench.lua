137ec2f3b4212e6b830059dc5f63a6f95e0143a9
blanhhy <balancestar676@qq.com>
2026-09-14 17:39:16 +0800

    perf(bench): add super constructor call performance test

--- tests/performance/bench.lua
+++ tests/performance/bench.lua
@@ -65,6 +65,7 @@ class "BenchCtorChild_7b01" : extends "BenchCtorSelf_7b01" {
 }
 
 class "BenchSuperBase_7b01" {
+    __init = function(self) self._v = 1 end;
     visit = function(self) return self end;
 }
 
@@ -74,6 +75,10 @@ class "BenchSuperSub_7b01" : extends "BenchSuperBase_7b01" {
     end;
     via_super0 = function(self)
         return super():visit()
+    end;
+    init_super = function(self)
+        super(BenchCtorChild_7b01, self)() -- 构造器专线
+        return self
     end;
     via_direct = function(self)
         return BenchSuperBase_7b01.visit(self)
@@ -204,6 +209,8 @@ bench('sub:via_super_x() [explicit]',
     function() acc = acc + (sub:via_super_x() == sub and 1 or 0) end, ref_super)
 bench('sub:via_super0() [0-arg]',
     function() acc = acc + (sub:via_super0() == sub and 1 or 0) end, ref_super)
+bench('sub:init_super() [call]',
+    function() acc = acc + (sub:init_super() == sub and 1 or 0) end, ref_super)
 
 print('\n== 偶发 API（绝对值）==')
 bench('Leaf:isExtends(Base) [true]',
