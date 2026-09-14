684b6144c07251254056fdd38d992261e2730e67
blanhhy <balancestar676@qq.com>
2026-09-14 22:02:23 +0800

    chore: 同步

--- simpleclass.lua
+++ simpleclass.lua
@@ -413,39 +413,36 @@ function I:__tostring()
     :format(self.__iname or '?')
 end
 
+local function extend(I1, I2, seen)
+    local v for i = 1, #I2 do
+        v = I2[i]
+        if not seen[v] then
+        seen[v] = true
+        I1[#I1+1] = v
+    end end
+    return I1
+end
+
 function I:extends(...)
     if M.I_FEATURE == "lexical" then return self end
-    local bases = {...}
-    local iface, mname
+    local bases, iface = {...}, nil
     for j = 1, #bases do
         iface = bases[j]
         if type(iface) ~= "table" or not iface.__iname then
             error(("bad interface extends: interface expected, got %s at #%d"):
             format(iface, j), 2)
         end
-        for i = 1, #iface do
-            mname = iface[i]
-            if not self[mname] then
-            self[#self+1] = mname
-            self[mname] = true
-        end end
+        extend(self, iface, self)
     end
     return self
 end
 
-function I:__call(mnames)
-    if type(mnames) ~= "table" then
+function I:__call(body)
+    if type(body) ~= "table" then
         error("interface cannot instantiate", 2)
     end
     if M.I_FEATURE == "lexical" then return self end
-    local mname
-    for i = 1, #mnames do
-        mname = mnames[i]
-        if not self[mname] then
-        self[#self+1] = mname
-        self[mname] = true
-    end end
-    return self
+    return extend(self, body, self)
 end
 
 function I:check_impl(clazz)
@@ -457,20 +454,12 @@ function I:check_impl(clazz)
     return true
 end
 
-function interface(name)
+function M.interface(name)
     if M.I_FEATURE == "lexical" then return setmetatable({}, I) end
     local typ = type(name)
     if typ == "table" then
-        local iface = name
-        local count = 0
-        for i = 1, #iface do
-            local mname = iface[i]
-            if not iface[mname] then
-            iface[count+1] = mname
-            iface[mname] = true
-            count = count + 1
-        end end
-        iface.__iname = "<anonymous>"
+        local iface = {__iname = "<anonymous>"}
+        extend(iface, name, iface)
         return setmetatable(iface, I)
     elseif typ ~= "string" or name == "" then
         return setmetatable({
@@ -478,10 +467,10 @@ function interface(name)
         }, I)
     end
     local iface = {__iname = name}
-    if M.AUTO_GLOBAL and (nil == G[name] or sc_ENV[name] == G[name]) then
+    if M.AUTO_GLOBAL and (nil == G[name] or M._ENV[name] == G[name]) then
         G[name] = iface
     end
-    sc_ENV[name] = iface
+    M._ENV[name] = iface
     return setmetatable(iface, I)
 end
 
