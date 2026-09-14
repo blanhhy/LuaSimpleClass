81833adc36d9316cd9bd45e46aa171e8165a7e74
blanhhy <balancestar676@qq.com>
2026-09-14 15:31:47 +0800

    fix: 优化clone的实现，消除闭包创建开销

--- simpleclass/object.lua
+++ simpleclass/object.lua
@@ -72,37 +72,38 @@ function object:isInstance(T)
     return false
 end
 
+local function deep(src, cls, seen)
+    if seen[src] then return seen[src] end
+    local c = {}
+    seen[src] = c
+    for k, v in next, src do
+        c[k] = (k == "__class" and v == cls) and cls
+            or (type(v) == "table") and deep(v, rawgetmt(v), seen)
+            or v
+    end
+    rawsetmt(c, cls)
+    return c
+end
+
+---Clone the object
+---@param isDeep? boolean Default `true`
+---@return object
+function object:clone(isDeep)
+    local clazz = rawgetmt(self)
+    if isDeep == nil or isDeep then
+        return deep(self, clazz, {})
+    end
+    local clone = {}
+    for k, v in next, self do clone[k] = v end
+    rawsetmt(clone, clazz)
+    return clone
+end
+
 setmetatable(object, M._CMT)
 M._ENV.object = object
 
 M.object = object
 M.isinstance = object.isInstance
 M.issubclass = object.isExtends
-
----Clone the object
----@param isDeep? boolean Default `true`
----@return object
-function object:clone(isDeep)
-    isDeep = isDeep == nil and true or isDeep
-    local seen = {} -- 源表 -> 克隆表，用于环检测与共享引用一致性
-    local function copy(src, clazz)
-        local done = seen[src]
-        if done then return done end
-        local c = {}
-        seen[src] = c
-        for k, v in next, src do
-            if k == "__class" and v == clazz then
-                c[k] = clazz
-            elseif isDeep and type(v) == "table" then
-                c[k] = copy(v, rawgetmt(v))
-            else
-                c[k] = v
-            end
-        end
-        rawsetmt(c, clazz)
-        return c
-    end
-    return copy(self, rawgetmt(self))
-end
 
 return object
