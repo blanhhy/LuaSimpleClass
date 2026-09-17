15f8f8c88c08e452559026c10d70356a71f8b6b1
blanhhy <balancestar676@qq.com>
2026-09-17 13:21:03 +0800

    chore: 调整注释

--- simpleclass/m.lua
+++ simpleclass/m.lua
@@ -35,8 +35,7 @@ function M.index(this, key, super)
     local clazz = this.__class or this
     if super then clazz = clazz["__base"] end
     local field
-    -- 预判实际 OOP 工程中可能出现的最大继承长度（一般 8 层，这里 12 层）
-    -- 使 LuaJIT 现在**可以编译**继承方法查找，大幅提升 LuaJIT 下的运行时性能
+    -- 预判实际 OOP 工程中可能出现的最大继承长度供 LuaJIT Trace 优化
     if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"] if not clazz then return end field = clazz[key] if field ~= nil then return field end clazz = clazz["__base"]
     -- 更深的链回落成迭代：只是慢，语义完全一致
     while clazz do
